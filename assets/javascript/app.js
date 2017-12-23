55b00c0b5218510d0b3afbf4622ed4f98a44c8e6
Jenny Valle <jennyvalle@Jennys-MacBook-Air.local>
2017-12-23 13:35:19 -0500

    Javascript begin

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+console.log("hot poppers");
+
+$(document).on("ready",function(){
+
+	console.log("ready eddy");
+
+	$("#title-window").appendTo("#display-window");
+	$("#trivia-window").appendTo("#hidden");
+	$("#score-window").appendTo("#hidden");
+
+});
