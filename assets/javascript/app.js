8d8bb9eede142afef3e74fe1017726d5d4ddaf1a
Jenny Valle <jennyvalle@Jennys-MacBook-Air.local>
2017-12-23 13:44:36 -0500

    Javascript II

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,9 +3,18 @@ console.log("hot poppers");
 $(document).on("ready",function(){
 
 	console.log("ready eddy");
+	var timer;
 
+	// Sets the windows in the appropriate places
 	$("#title-window").appendTo("#display-window");
 	$("#trivia-window").appendTo("#hidden");
 	$("#score-window").appendTo("#hidden");
+
+	$("#click-here").on("click",function(){
+
+		$("#title-window").appendTo("#hidden");
+		$("#trivia-window").appendTo("#display-window");
+
+	});
 
 });
