49d115e7e1cad54ffa09380833de65310d857a0d
Jenny Valle <jennyvalle@Jennys-MacBook-Air.local>
2017-12-23 14:41:12 -0500

    Added answering and timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,7 @@
-console.log("hot poppers");
+// The trivia questions
 
 const triviaQuestions = [
+
 	{
 		question: `Which of these comics legends created Captain America, the Human Torch, the X-Men and invented the genre of Romance comics?`,
 		answers : [
@@ -20,45 +21,78 @@ const triviaQuestions = [
 					 name: "Don Heck",
 					 correct: false
 					}
-				 ]
+				 ],
+		correctAnswer: ``,
+		correctImg: ``
 	}
 ];
 
-// Adds a trivia question based on the index to the window.
-
+// Adds a trivia question based on the index to the and returns the index of the correct answer.
 
 
 function sendQuestion(ind){
 	
 	$("#question").html(triviaQuestions[ind].question);
 
-	for( let i = 0 ; i < 4 ; ++i ){
-		$("#answer" + i).attr("iscorrect",triviaQuestions[ind].answers[i].correct)
-					    .text(triviaQuestions[ind].answers[i].name);
-	}
+	let correct = -1;
 
+	for( let i = 0 ; i < 4 ; ++i ){
+		$("#answer" + i).text(triviaQuestions[ind].answers[i].name);
+		if(triviaQuestions[ind].answers[i].correct)
+			correct = i;
+	}
+	return correct;
 }
 
 $(document).on("ready",function(){
 
+
+	let timer;
+	let time = 30;
+
+	// Sets the timer interval;
+
+	function startTimer(){
+		timer = setInterval(()=>{
+
+			--time;
+			$("#time").text(time);
+
+			if(time === 0)
+				stopTimer(-1);
+			
+		},1000);
+	}
+
+	// Stops the timer with an answer 
+	function stopTimer(ans){
+
+		clearInterval(timer);
+		console.log("you answered " + ans);
+
+	}
+
 	console.log("ready eddy");
-	var timer;
 
 	// Sets the windows in the appropriate places
+
 	$("#title-window").appendTo("#display-window");
 	$("#trivia-window").appendTo("#hidden");
 	$("#score-window").appendTo("#hidden");
+
+	// Starts the game
 
 	$("#click-here").on("click",function(){
 
 		$("#title-window").appendTo("#hidden");
 		$("#trivia-window").appendTo("#display-window");
 		sendQuestion(0);
+		startTimer();
 
 	});
 
 	$(".answer").on("click",function(){
-		console.log();
+		stopTimer(this.id.split("r")[1]);
 	});	
 
 });
