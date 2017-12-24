b05f0c256793396ad04fd92248d3c63defeab4dc
Jenny Valle <jennyvalle@Jennys-MacBook-Air.local>
2017-12-24 13:22:57 -0500

    Added some questions. Did some fiddling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -22,205 +22,209 @@ const triviaQuestions = [
 					 correct: false
 					}
 				 ],
-		correctAnswer: `<h3 id="ans-title"><strong>Jack Kirby</strong> the King Of Comics</h3><p>Stan Lee didn't have anything to do with creating Captain America. Heck, his first published comics work was a short prose story in Captain America Comics #7</p>`,
+		correctAnswer: `<h2 id="ans-title"><strong>Jack Kirby</strong> the King Of Comics</h2><p>Stan Lee didn't have anything to do with creating Captain America. Heck, his first published comics work was a short prose story in Captain America Comics #7</p>`,
 		correctImg: ``
 	},{
-		question: `How Many?`,
+		question: `Which of the following single issues is considered to be the first comic that collectors bought speculating it would go up in value on it's initial release?`,
 		answers : [
 					{
-					 name: "Five",
+					 name: "Crisis on Infinite Earths #7 (1985)",
 					 correct: false
 					},
 					{
-					 name: "Seven",
+					 name: "Superman #75 (1993)",
+					 correct: false
+					},
+					{
+					 name: "Howard the Duck #1 (1976)",
 					 correct: true
 					},
 					{
-					 name: "None",
-					 correct: false
-					},
-					{
-					 name: "Negative One",
+					 name: "Fantastic Four #1 (1961)",
 					 correct: false
 					}
 				 ],
-		correctAnswer: ``,
+		correctAnswer: `<h2 id="ans-title">Howard the Duck #1</h2>
+						<p>Our fine feathered friend <strong>Howard the Duck</strong>! One of the first #1 issues that was released after publishers started shipping titles directly to pecialty comic shops in the 1970s.</p>`,
 		correctImg: ``
 	},{
-		question: `Which of these comics legends created Captain America, the Human Torch, the X-Men and invented the genre of Romance comics?`,
+		question: `Comics sure have had a lot of wacky crossovers. Say, which one of these four hasn't actually seen print?`,
 		answers : [
 					{
-					 name: "Jack Kirby",
+					 name: "Archie vs Predator",
+					 correct: false
+					},
+					{
+					 name: "Superman Planet of the Apes",
 					 correct: true
 					},
 					{
-					 name: "Stan Lee",
+					 name: "The Flash and Colonel Sanders",
 					 correct: false
 					},
 					{
-					 name: "Wally Wood",
+					 name: "Star Trek and Doctor Who",
 					 correct: false
-					},
+					}
+				 ],
+		correctAnswer: `<h2 id="ans-title">Superman Planet of the Apes</h2>
+						<p>While Green Lantern may have visted the Planet of the Apes, Superman hasn't! Though definitely make sure to read that Archie vs Predator book, its pretty incredible.</p>`,
+		correctImg: ``
+	},{
+		question: `With the help of Will Murray, which one of these classic Marvel creators returned to the company after a long haitus to pencil the first appearance of Squirrel Girl?`,
+		answers : [
 					{
 					 name: "Don Heck",
 					 correct: false
+					},
+					{
+					 name: "Neal Adams",
+					 correct: false
+					},
+					{
+					 name: "Steranko",
+					 correct: false
+					},
+					{
+					 name: "Steve Ditko",
+					 correct: true
 					}
 				 ],
-		correctAnswer: ``,
+		correctAnswer: `<h2 id="ans-title">Steve Ditko!</h2>
+						<p>That's right, the original artist for Spider-Man <strong>Steve Ditko</strong> returned to Marvel to bring his classic art to an amazing story!</p>`,
 		correctImg: ``
 	},{
-		question: `Which of these comics legends created Captain America, the Human Torch, the X-Men and invented the genre of Romance comics?`,
+		question: `...speaking of classic Marvel creators--which old school Marvel employee was famously lampooned as Funky Flashman, an overblown self promoter first appearing in Jack Kirby's Mister Miracle #6?`,
 		answers : [
 					{
-					 name: "Jack Kirby",
+					 name: "Stan Lee",
 					 correct: true
 					},
 					{
-					 name: "Stan Lee",
+					 name: "Gene Colon",
 					 correct: false
 					},
 					{
-					 name: "Wally Wood",
+					 name: "Roy Thomas",
 					 correct: false
 					},
 					{
-					 name: "Don Heck",
+					 name: "John Buscema",
 					 correct: false
 					}
 				 ],
-		correctAnswer: ``,
+		correctAnswer: `<h2 id="ans-title">Stan "The Man" Lee</h2>
+						<p>It's no secret that Kirby resented Lee for promoting himself solely based on the talent of Kirby's tireless work at Marvel with nothing to show for it. So when Kirby left for DC Comics (where he got full creative control over his books) he thought he'd throw a little shade. Maybe it should sy Stan the Con Man Lee instead!</p>`,
 		correctImg: ``
 	},{
-		question: `Which of these comics legends created Captain America, the Human Torch, the X-Men and invented the genre of Romance comics?`,
+		question: `So we've spent a lot of time talking about Marvel--let's talk DC! Which of the following mysical characters first debuted in the pages of <i>Hawkman</i>?`,
 		answers : [
 					{
-					 name: "Jack Kirby",
+					 name: "John Constantine",
+					 correct: false
+					},
+					{
+					 name: "Sargon the Sorcerer",
+					 correct: false
+					},
+					{
+					 name: "Zatanna",
 					 correct: true
 					},
 					{
-					 name: "Stan Lee",
-					 correct: false
-					},
-					{
-					 name: "Wally Wood",
-					 correct: false
-					},
-					{
-					 name: "Don Heck",
+					 name: "Black Alice",
 					 correct: false
 					}
 				 ],
-		correctAnswer: ``,
+		correctAnswer: `<h2 id="ans-title">Zatanna</h2><p>That's right everyone's favorite backward speaking magician <strong>Zatanna</strong> made her first appearance in 1965's Hawkman #4. Though maybe she's changed just a bit since then!</p>`,
 		correctImg: ``
 	},{
-		question: `Which of these comics legends created Captain America, the Human Torch, the X-Men and invented the genre of Romance comics?`,
+		question: ``,
 		answers : [
 					{
-					 name: "Jack Kirby",
+					 name: "",
 					 correct: true
 					},
 					{
-					 name: "Stan Lee",
+					 name: "",
 					 correct: false
 					},
 					{
-					 name: "Wally Wood",
+					 name: "",
 					 correct: false
 					},
 					{
-					 name: "Don Heck",
+					 name: "",
 					 correct: false
 					}
 				 ],
-		correctAnswer: ``,
+		correctAnswer: `<h2 id="ans-title"></h2><p></p>`,
 		correctImg: ``
 	},{
-		question: `Which of these comics legends created Captain America, the Human Torch, the X-Men and invented the genre of Romance comics?`,
+		question: ``,
 		answers : [
 					{
-					 name: "Jack Kirby",
+					 name: "",
 					 correct: true
 					},
 					{
-					 name: "Stan Lee",
+					 name: "",
 					 correct: false
 					},
 					{
-					 name: "Wally Wood",
+					 name: "",
 					 correct: false
 					},
 					{
-					 name: "Don Heck",
+					 name: "",
 					 correct: false
 					}
 				 ],
-		correctAnswer: ``,
+		correctAnswer: `<h2 id="ans-title"></h2><p></p>`,
 		correctImg: ``
 	},{
-		question: `Which of these comics legends created Captain America, the Human Torch, the X-Men and invented the genre of Romance comics?`,
+		question: ``,
 		answers : [
 					{
-					 name: "Jack Kirby",
+					 name: "",
 					 correct: true
 					},
 					{
-					 name: "Stan Lee",
+					 name: "",
 					 correct: false
 					},
 					{
-					 name: "Wally Wood",
+					 name: "",
 					 correct: false
 					},
 					{
-					 name: "Don Heck",
+					 name: "",
 					 correct: false
 					}
 				 ],
-		correctAnswer: ``,
+		correctAnswer: `<h2 id="ans-title"></h2><p></p>`,
 		correctImg: ``
 	},{
-		question: `Which of these comics legends created Captain America, the Human Torch, the X-Men and invented the genre of Romance comics?`,
+		question: ``,
 		answers : [
 					{
-					 name: "Jack Kirby",
+					 name: "",
 					 correct: true
 					},
 					{
-					 name: "Stan Lee",
+					 name: "",
 					 correct: false
 					},
 					{
-					 name: "Wally Wood",
+					 name: "",
 					 correct: false
 					},
 					{
-					 name: "Don Heck",
+					 name: "",
 					 correct: false
 					}
 				 ],
-		correctAnswer: ``,
-		correctImg: ``
-	},{
-		question: `Which of these comics legends created Captain America, the Human Torch, the X-Men and invented the genre of Romance comics?`,
-		answers : [
-					{
-					 name: "Jack Kirby",
-					 correct: true
-					},
-					{
-					 name: "Stan Lee",
-					 correct: false
-					},
-					{
-					 name: "Wally Wood",
-					 correct: false
-					},
-					{
-					 name: "Don Heck",
-					 correct: false
-					}
-				 ],
-		correctAnswer: ``,
+		correctAnswer: `<h2 id="ans-title"></h2><p></p>`,
 		correctImg: ``
 	}
 ];
@@ -281,11 +285,13 @@ $(document).on("ready",function(){
 	// Shows the final stats at the end of the game 
 
 	function showTotals(){
+
 		hide("answer-window");
 		show("totals-window");
 		$("#correct").text(correct);
 		$("#incorrect").text(incorrect);
 		$("#unanswered").text(unanswered);
+
 	}
 
 	// Sets the timer interval. Stops the time with an answer of "-1" if 30 seconds has elapsed
@@ -322,10 +328,12 @@ $(document).on("ready",function(){
 	// Hides the trivia question window and shows the correct answer and answer image before sending the next question
 
 	function showAnswer(correct){
+		
 		if(correct)
 			$("#answer-text").text("Correct!");
 		else
 			$("#answer-text").text("Incorrect! :(");
+
 		$("#answer-exp").html(triviaQuestions[currentQuestion].correctAnswer);
 		hide("trivia-window");
 		show("answer-window");
@@ -340,7 +348,9 @@ $(document).on("ready",function(){
 				hide("answer-window");
 				nextQuestion();
 
-			},3000);
+			},8000);
+
+		// If the user has made it through all four rounds it shows the totals screen
 
 		} else {
 
